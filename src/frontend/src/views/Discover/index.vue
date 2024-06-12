9d8b751af610f746c69c9721b148e001e29d551e
初十 <392706283@qq.com>
2024-06-12 21:16:40 +0800

    代码提交

--- src/frontend/src/views/Discover/index.vue
+++ src/frontend/src/views/Discover/index.vue
@@ -1,29 +1,4 @@
 <template>
-  <el-menu :default-active="active" mode="horizontal" :router="true">
-    <el-menu-item v-for="route in discoverRoutes" :index="`/discover/${route.path}`">{{route.meta.title}}</el-menu-item>
-  </el-menu>
-  <el-scrollbar view-style="padding: 0 20px;">
-    <router-view />
-  </el-scrollbar>
+  为我推荐
 </template>
 
-<script setup lang="ts">
-import {useRoute} from "vue-router";
-import {computed} from "vue";
-import discoverRoutes from "@/router/discover";
-
-const currentRoute = useRoute()
-
-const active = computed(() => {
-  const currentPath = currentRoute.path.replace('/discover', '').split('/')
-  const activeRoute = discoverRoutes.find(item => item.path === currentPath[1])
-  if (activeRoute) return `/discover/${activeRoute.path}`
-  return ''
-})
-</script>
-
-<style scoped lang="scss">
-.el-scrollbar {
-  height: calc(100% - 58px);
-}
-</style>
--- src/frontend/src/views/Video/index.vue
+++ src/frontend/src/views/Video/index.vue
@@ -1,30 +1,4 @@
 <template>
-  <el-menu :default-active="active" mode="horizontal" :router="true">
-    <el-menu-item v-for="route in videoRoutes" :index="`/video/${route.path}`">{{route.meta.title}}</el-menu-item>
-  </el-menu>
-  <el-scrollbar view-style="padding: 0 20px;">
-    <router-view />
-  </el-scrollbar>
+   教学视频
 </template>
 
-<script setup lang="ts">
-import {useRoute} from "vue-router";
-import {computed} from "vue";
-import videoRoutes from "@/router/video";
-
-const currentRoute = useRoute()
-
-const active = computed(() => {
-  // 根据第二级路由path判断当前激活的路由
-  const currentPath = currentRoute.path.replace('/video', '').split('/')
-  const activeRoute = videoRoutes.find(item => item.path === currentPath[1])
-  if (activeRoute) return `/video/${activeRoute.path}`
-  return ''
-})
-</script>
-
-<style scoped lang="scss">
-.el-scrollbar {
-  height: calc(100% - 58px);
-}
-</style>
