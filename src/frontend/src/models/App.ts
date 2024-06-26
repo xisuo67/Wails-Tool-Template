f36cd09a77b73eceed302a1086edd1fafbba04a6
初十 <392706283@qq.com>
2024-06-26 22:30:24 +0800

    调整代码结构

--- src/frontend/src/models/App.ts
+++ src/frontend/src/models/App.ts
@@ -1,66 +1,7 @@
-import {Song} from "@/models/Song";
-
 export interface AppList {
     name: string
     id: number
-    userId: number
-    coverImgUrl: string
-    coverImgId: number
-    createTime: number
-    updateTime: number
-    description: string
-    tags: Array<string>
-    tag?: string
-    trackUpdateTime: number
-    trackCount: number
-    playCount: number
-    specialType: number
-    totalDuration: number
-    creator: Creator | null
-    tracks: Array<Song> | any
-    subscribed: boolean
-    subscribers: []
-    subscribedCount: number
-    commentThreadId: string
-    newImported: boolean
-    adType: number
-    highQuality: boolean
-    privacy: number
-    ordered: boolean
-    shareCount?: number
-    commentCount?: number
-    copywriter?: string
 }
-
-interface Creator {
-    followed: boolean
-    avatarUrl: string
-    avatarImgId: number
-    city: number
-    birthday: number
-    userId: number
-    userType: number
-    nickname: string
-    signature: string
-    description: string
-    detailDescription: string
-    backgroundImgId: number
-    backgroundUrl: string
-    avatarDetail: {
-        identityIconUrl: string
-        identityLevel: number
-        userType: number
-    }
-    anchor: boolean
-}
-
-export interface TopList extends AppList{
-    tracks: Array<{
-        first: string;
-        second: string;
-    }>
-}
-
 export interface PersonalizedAppList {
     id: number
     type: number
